a7f98a7379643e942cadccd396b8ad5a8bdae3c5
Rex <proandceo@gmail.com>
2023-03-15 14:14:38 +1000

    Update stats.py

--- ssg/extensions/stats.py
+++ ssg/extensions/stats.py
@@ -4,7 +4,17 @@ import time
 start_time = None
 total_written = 0
 
-@property("start_build")
+@hooks.register("start_build")
 def start_build():
     global start_time
-    
+    start_time = time.time()
+
+
+@hooks.register("written")
+def written():
+    global  total_written
+    total_written += 1
+
+@hooks.register("stats")
+def stats():
+    final_time = time.time() - start_time
