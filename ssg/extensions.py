e2db14ff51d8bd783972dc5065b9166a56265fe5
Rex <proandceo@gmail.com>
2023-03-15 13:05:17 +1000

    Update extensions.py

--- ssg/extensions.py
+++ ssg/extensions.py
@@ -7,3 +7,12 @@ def load_module(directory, name):
     sys.path.insert(0, directory)
     importlib.import_module(name)
     sys.path.pop(0)
+
+def load_directory(directory):
+    for path in directory:
+        Path.rglob(".py")
+        load_module(directory.as_posix(), path.stem)
+
+def load_bundled():
+    directory = Path(__file__).parent + "/extensions"
+    load_bundled(load_directory(directory))
